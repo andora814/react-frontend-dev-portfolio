34fa5d66d9df97daa42d1ecc8674c9dbc84a72be
andora814 <smith.doran@gmail.com>
2022-02-22 20:24:43 -0500

    Updates

---
+++ precache-manifest.0177c1fae5c2260bac9c3b30f2c2266b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1742cec55e2d6df3e3919a829de166f6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2d0c0fda73911767d8d3",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f38aea6071d4f544a015",
+    "url": "/portfolio/static/css/main.bf8f3013.chunk.css"
+  },
+  {
+    "revision": "2d0c0fda73911767d8d3",
+    "url": "/portfolio/static/js/2.e27924fd.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.e27924fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38aea6071d4f544a015",
+    "url": "/portfolio/static/js/main.cef11574.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
